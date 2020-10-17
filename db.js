4c547569d2580971ba61e177fc9c4efef6912ea6
slavikshakhov <v.shakhov@yahoo.ca>
2020-10-17 13:11:32 +0200

    deployment2

--- db.js
+++ db.js
@@ -14,9 +14,9 @@ const db = mysql.createConnection({
   user: "b690bd5185aff9",
   password: "1815f368",
   database: "heroku_e7f976b84f567b9",
+  port: 3306
 });
 
-//mysql://b690bd5185aff9:1815f368@eu-cdbr-west-03.cleardb.net/heroku_e7f976b84f567b9?reconnect=true
 // Connect
 db.connect((err) => {
   if (err) {
